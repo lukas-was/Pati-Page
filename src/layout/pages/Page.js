ec98afb6c7e735fcd24f04e40b8fc8fcda1f517f
Lukas Was <wetsand@o2.pl>
2020-11-04 20:30:07 +0100

    change function component Page for class component

--- src/layout/pages/Page.js
+++ src/layout/pages/Page.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useRef } from "react";
+import React, { Component } from "react";
+
 import "../../style/Page.css";
 
-const Page = () => {
-  let valueZ = 0;
+const animationTime = 7000;
 
-  // const container = useRef();
+class Page extends Component {
+  valueZ = 0;
 
-  const getPosition = (e) => {
+  getPosition = (e) => {
     const windowHeight = window.innerHeight;
     const windowWidth = window.innerWidth;
 
@@ -23,9 +24,9 @@ const Page = () => {
     return { xProcent, yProcent };
   };
 
-  const setPosition = (e) => {
+  setPosition = (e) => {
     const container = document.querySelector(".image_container");
-    const position = getPosition(e);
+    const position = this.getPosition(e);
 
     const X = position.xProcent / 20;
     const Y = position.yProcent / 10;
@@ -33,51 +34,139 @@ const Page = () => {
     container.style.transform = `translate(${-50 - X}%, ${-50 + Y}%)`;
   };
 
-  const zoomInAndOut = (e) => {
+  zoomInAndOut = (e) => {
     const images = [...document.querySelectorAll(".image_container div")];
 
-    if (e.deltaY === 100 && valueZ < 120) valueZ += 15;
-    if (e.deltaY === -100 && valueZ > 0) valueZ -= 15;
+    if (e.deltaY === 100 && this.valueZ < 120) this.valueZ += 15;
+    if (e.deltaY === -100 && this.valueZ > 0) this.valueZ -= 15;
 
     images.forEach(
-      (image) => (image.style.transform = `translateZ(${valueZ}px)`)
+      (image) => (image.style.transform = `translateZ(${this.valueZ}px)`)
     );
   };
 
-  useEffect(() => {
-    window.addEventListener("mousemove", setPosition);
-    window.addEventListener("mousewheel", zoomInAndOut);
-    return () => {
-      window.removeEventListener("mousemove", setPosition);
-      window.removeEventListener("mousewheel", zoomInAndOut);
-    };
-  });
+  componentDidMount() {
+    window.addEventListener("mousemove", this.setPosition);
+    this.indexTime = setTimeout(() => {
+      window.addEventListener("mousewheel", this.zoomInAndOut);
+    }, animationTime);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("mousemove", this.setPosition);
+    window.removeEventListener("mousewheel", this.zoomInAndOut);
+    clearTimeout(this.indexTime);
+  }
 
-  return (
-    <div className="image_container">
-      <div className="image"></div>
-      <div className="image"></div>
-      <div className="imageS"></div>
-      <div className="imageS"></div>
-      <div className="image"></div>
-      <div className="image"></div>
-      <div className="image"></div>
-      <div className="imageS"></div>
-      <div className="image"></div>
-      <div className="imageS"></div>
-      <div className="imageS"></div>
-      <div className="image"></div>
-      <div className="image"></div>
-      <div className="image"></div>
-      <div className="imageS"></div>
-      <div className="imageS"></div>
-      <div className="imageS"></div>
-      <div className="image"></div>
-      <div className="image"></div>
-      <div className="imageS"></div>
-      <div className="imageS"></div>
-    </div>
-  );
-};
-
+  render() {
+    return (
+      <div className="image_container">
+        <div className="image"></div>
+        <div className="image"></div>
+        <div className="imageS"></div>
+        <div className="imageS"></div>
+        <div className="image"></div>
+        <div className="image"></div>
+        <div className="image"></div>
+        <div className="imageS"></div>
+        <div className="image"></div>
+        <div className="imageS"></div>
+        <div className="imageS"></div>
+        <div className="image"></div>
+        <div className="image"></div>
+        <div className="image"></div>
+        <div className="imageS"></div>
+        <div className="imageS"></div>
+        <div className="imageS"></div>
+        <div className="image"></div>
+        <div className="image"></div>
+        <div className="imageS"></div>
+        <div className="imageS"></div>
+      </div>
+    );
+  }
+}
 export default Page;
+
+// import React, { Component } from "react";
+// import "../../style/Page.css";
+
+// const Page = () => {
+//   let valueZ = 0;
+
+//   const getPosition = (e) => {
+//     const windowHeight = window.innerHeight;
+//     const windowWidth = window.innerWidth;
+
+//     const x = e.clientX;
+//     const y = e.clientY;
+
+//     const oX = Math.floor(-(windowWidth / 2 - x));
+//     const oY = Math.floor(windowHeight / 2 - y);
+
+//     const xProcent = Math.floor((oX / (windowWidth * 0.5)) * 100);
+
+//     const yProcent = Math.floor((oY / (windowHeight * 0.5)) * 100);
+
+//     return { xProcent, yProcent };
+//   };
+
+//   const setPosition = (e) => {
+//     const container = document.querySelector(".image_container");
+//     const position = getPosition(e);
+
+//     const X = position.xProcent / 20;
+//     const Y = position.yProcent / 10;
+
+//     container.style.transform = `translate(${-50 - X}%, ${-50 + Y}%)`;
+//   };
+
+//   const zoomInAndOut = (e) => {
+//     const images = [...document.querySelectorAll(".image_container div")];
+
+//     if (e.deltaY === 100 && valueZ < 120) valueZ += 15;
+//     if (e.deltaY === -100 && valueZ > 0) valueZ -= 15;
+
+//     images.forEach(
+//       (image) => (image.style.transform = `translateZ(${valueZ}px)`)
+//     );
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener("mousemove", setPosition);
+//     setTimeout(() => {
+//       window.addEventListener("mousewheel", zoomInAndOut);
+//     }, 7000);
+//     return () => {
+//       window.removeEventListener("mousemove", setPosition);
+//       window.removeEventListener("mousewheel", zoomInAndOut);
+//     };
+//   });
+
+//   return (
+//     <div className="image_container">
+//       <div className="image"></div>
+//       <div className="image"></div>
+//       <div className="imageS"></div>
+//       <div className="imageS"></div>
+//       <div className="image"></div>
+//       <div className="image"></div>
+//       <div className="image"></div>
+//       <div className="imageS"></div>
+//       <div className="image"></div>
+//       <div className="imageS"></div>
+//       <div className="imageS"></div>
+//       <div className="image"></div>
+//       <div className="image"></div>
+//       <div className="image"></div>
+//       <div className="imageS"></div>
+//       <div className="imageS"></div>
+//       <div className="imageS"></div>
+//       <div className="image"></div>
+//       <div className="image"></div>
+//       <div className="imageS"></div>
+//       <div className="imageS"></div>
+//     </div>
+//   );
+// };
+
+// export default Page;
